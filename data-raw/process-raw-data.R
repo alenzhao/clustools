3a1b1da43df78efd337f3fea6467d04795f7ae89
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2015-06-22 18:04:10 +0100

    minor change

--- data-raw/process-raw-data.R
+++ data-raw/process-raw-data.R
@@ -1,7 +1,8 @@
 # Biase, F. H., Cao, X. & Zhong, S. Cell fate inclination within 2-cell and
 # 4-cell mouse embryos revealed by single-cell RNA sequencing. Genome Res. 24,
 # 1787–1796 (2014).
-zhong <- read.table("inst/extdata/zhong.txt", header = T)
+# http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE57249
+zhong <- read.table("inst/extdata/GSE57249_fpkm.txt", header = T)
 zhong <- as.matrix(zhong[ , 2:50])
 colnames(zhong) <- c(rep(1, 9), rep(2, 20), rep(3, 20))
 save(zhong, file = "data/zhong.rda")
