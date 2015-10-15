dc5fcd4c889ed7bf89e9f67936f1edd2299e8077
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2015-10-15 21:34:03 +0100

    add pca_reduce()

---
+++ R/pcaReduce.R
@@ -0,0 +1,18 @@
+pca_reduce <- function(dataset) {
+    Input <- t(log2(dataset + 1)) # data matrix, cells in rows, genes in columns
+    true_labs <- rownames(Input)
+    Output_S <- PCAreduce(Input, nbt=100, q=30, method='S')
+    N <- length(Output_S)
+    M <- dim(Output_S[[1]])[2]
+    K <- c()
+    for (n in 1:N){
+        cls_cell <- c()
+        labels <- c()
+        for (m in 1:M){
+            cls_cell <- c(cls_cell, adjustedRandIndex(Output_S[[n]][,m], true_labs))
+            labels <- c(labels, length(unique(Output_S[[n]][,m])))
+        }
+        K <- cbind(K, cls_cell)
+    }
+    return(K[which(labels == length(unique(rownames(Input)))), ])
+}
