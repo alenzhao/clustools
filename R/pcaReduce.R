2519764b2828dac6d384cbf0bfe4fc556a9159d9
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2015-10-16 09:54:59 +0100

    adapt pcaReduce for bernstein dataset

--- R/pcaReduce.R
+++ R/pcaReduce.R
@@ -1,5 +1,10 @@
 pca_reduce <- function(dataset) {
-    Input <- t(log2(dataset + 1)) # data matrix, cells in rows, genes in columns
+    if(deparse(substitute(dataset)) != "bernstein") {
+        Input <- t(log2(dataset + 1)) # data matrix, cells in rows, genes in columns
+    } else {
+        print("bernstein")
+        Input <- t(dataset) # data matrix, cells in rows, genes in columns
+    }
     true_labs <- rownames(Input)
     Output_S <- PCAreduce(Input, nbt=100, q=30, method='S')
     N <- length(Output_S)
